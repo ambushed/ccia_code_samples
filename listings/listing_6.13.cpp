97a837348d30810a54a3b845c0728cbd41bac536
ambushed <vlad.sergeev@gmail.com>
2023-10-11 09:10:40 +0200

    Update listing_6.13.cpp

--- listings/listing_6.13.cpp
+++ listings/listing_6.13.cpp
@@ -4,17 +4,17 @@
 template<typename T>
 class threadsafe_list
 {
-    struct node
+    struct node //1 - default constructed node 
     {
         std::mutex m;
         std::shared_ptr<T> data;
         std::unique_ptr<node> next;
 
-        node():
+        node(): // 2
             next()
         {}
         
-        node(T const& value):
+        node(T const& value): // 3
             data(std::make_shared<T>(value))
         {}
     };
@@ -35,29 +35,29 @@ public:
     
     void push_front(T const& value)
     {
-        std::unique_ptr<node> new_node(new node(value));
+        std::unique_ptr<node> new_node(new node(value)); // 4
         std::lock_guard<std::mutex> lk(head.m);
-        new_node->next=std::move(head.next);
-        head.next=std::move(new_node);
+        new_node->next=std::move(head.next); // 5
+        head.next=std::move(new_node); // 6
     }
 
     template<typename Function>
-    void for_each(Function f)
+    void for_each(Function f) // 7
     {
         node* current=&head;
-        std::unique_lock<std::mutex> lk(head.m);
-        while(node* const next=current->next.get())
+        std::unique_lock<std::mutex> lk(head.m); // 8
+        while(node* const next=current->next.get()) //9
         {
-            std::unique_lock<std::mutex> next_lk(next->m);
-            lk.unlock();
-            f(*next->data);
+            std::unique_lock<std::mutex> next_lk(next->m); // 10
+            lk.unlock(); // 11
+            f(*next->data); // 12
             current=next;
-            lk=std::move(next_lk);
+            lk=std::move(next_lk); // 13
         }
     }
 
     template<typename Predicate>
-    std::shared_ptr<T> find_first_if(Predicate p)
+    std::shared_ptr<T> find_first_if(Predicate p) // 14
     {
         node* current=&head;
         std::unique_lock<std::mutex> lk(head.m);
@@ -65,9 +65,9 @@ public:
         {
             std::unique_lock<std::mutex> next_lk(next->m);
             lk.unlock();
-            if(p(*next->data))
+            if(p(*next->data)) // 15
             {
-                return next->data;
+                return next->data; // 16
             }
             current=next;
             lk=std::move(next_lk);
@@ -76,22 +76,22 @@ public:
     }
 
     template<typename Predicate>
-    void remove_if(Predicate p)
+    void remove_if(Predicate p) // 17
     {
         node* current=&head;
         std::unique_lock<std::mutex> lk(head.m);
         while(node* const next=current->next.get())
         {
             std::unique_lock<std::mutex> next_lk(next->m);
-            if(p(*next->data))
+            if(p(*next->data)) // 18
             {
                 std::unique_ptr<node> old_next=std::move(current->next);
-                current->next=std::move(next->next);
+                current->next=std::move(next->next); // 19
                 next_lk.unlock();
-            }
+            } // 20
             else
             {
-                lk.unlock();
+                lk.unlock(); // 21
                 current=next;
                 lk=std::move(next_lk);
             }
